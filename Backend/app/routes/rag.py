d744a1445747998ceb16ff658610c2d21964e4b4
Dheemant <dheemantcode@gmail.com>
2026-06-22 15:19:11 +0530

    Completed Day 33 RAG chunking and vector store

--- Backend/app/routes/rag.py
+++ Backend/app/routes/rag.py
@@ -3,11 +3,49 @@ from fastapi import UploadFile
 from fastapi import File
 
 from pypdf import PdfReader
+import faiss
+import numpy as np
+
+from sentence_transformers import (
+    SentenceTransformer
+)
 
 router = APIRouter()
 
 knowledge_base = []
+model = SentenceTransformer(
+    "all-MiniLM-L6-v2"
+)
 
+vector_store = None
+
+stored_chunks = []
+def chunk_text(
+
+    text,
+    chunk_size=500
+
+):
+
+    chunks = []
+
+    for i in range(
+
+        0,
+        len(text),
+        chunk_size
+
+    ):
+
+        chunks.append(
+
+            text[
+                i:i+chunk_size
+            ]
+
+        )
+
+    return chunks
 
 @router.post("/upload-pdf")
 async def upload_pdf(
@@ -30,16 +68,46 @@ async def upload_pdf(
 
             text += extracted
 
-    knowledge_base.append(
-        text
+    chunks = chunk_text(
+    text
     )
+
+    embeddings = model.encode(
+        chunks
+    )
+
+    global vector_store
+    global stored_chunks
+
+    dimension = len(
+        embeddings[0]
+    )
+
+    vector_store = faiss.IndexFlatL2(
+        dimension
+    )
+
+    vector_store.add(
+
+        np.array(
+            embeddings
+        ).astype(
+            "float32"
+        )
+
+    )
+
+    stored_chunks = chunks
 
     return {
 
-        "message":
-        "PDF uploaded successfully",
+    "message":
+    "PDF processed",
 
-        "characters":
-        len(text)
+    "characters":
+    len(text),
 
-    }
+    "chunks":
+    len(chunks)
+
+}
