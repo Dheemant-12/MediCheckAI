2e777e3386232777ccb829f8249e8b03bcd63036
Dheemant <dheemantcode@gmail.com>
2026-06-28 20:59:53 +0530

    Completed Day 40 frontend RAG chat interface

--- Frontend/src/pages/RAG.jsx
+++ Frontend/src/pages/RAG.jsx
@@ -4,7 +4,12 @@ import axios from "axios"
 function RAG() {
 
   const [file, setFile] = useState(null)
+
   const [message, setMessage] = useState("")
+
+  const [question, setQuestion] = useState("")
+
+  const [answer, setAnswer] = useState("")
 
   const token = localStorage.getItem("token")
 
@@ -64,6 +69,56 @@ function RAG() {
 
   }
 
+  const askPDF = async () => {
+
+    if (!question) {
+
+      alert("Enter a question")
+
+      return
+
+    }
+
+    try {
+
+      const response =
+        await axios.post(
+
+          "http://127.0.0.1:8000/ask-pdf",
+
+          {
+
+            question
+
+          },
+
+          {
+
+            headers: {
+
+              Authorization:
+                `Bearer ${token}`
+
+            }
+
+          }
+
+        )
+
+      setAnswer(
+        response.data.answer
+      )
+
+    }
+
+    catch (error) {
+
+      console.error(error)
+
+    }
+
+  }
+
   return (
 
     <div
@@ -105,6 +160,58 @@ function RAG() {
         {message}
 
       </h3>
+
+      <br />
+
+      <textarea
+
+        placeholder="Ask something about the uploaded PDF..."
+
+        rows={5}
+
+        cols={70}
+
+        value={question}
+
+        onChange={(e) =>
+          setQuestion(
+            e.target.value
+          )
+        }
+
+      />
+
+      <br /><br />
+
+      <button
+        onClick={askPDF}
+      >
+
+        Ask PDF
+
+      </button>
+
+      <br /><br />
+
+      <div
+        style={{
+          border: "1px solid #ddd",
+          padding: "20px",
+          borderRadius: "10px",
+          minHeight: "120px",
+          whiteSpace: "pre-wrap"
+        }}
+      >
+
+        <strong>
+          AI Answer
+        </strong>
+
+        <br /><br />
+
+        {answer}
+
+      </div>
 
     </div>
 
